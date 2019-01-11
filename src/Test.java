db1563909d9954ae9c0ae031b47a7e0b6b2dec08
hkt <123@qq.com>
2019-01-11 12:41:12 +0800

    又增加了一行代码

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("好好学习，天天向上");
         System.out.println("no pains no gains");
+        System.out.println("words seems to descripe my sense");
     }
 }
